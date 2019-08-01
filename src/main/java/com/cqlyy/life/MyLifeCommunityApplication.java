a07b991cf346e0b8f1f2f6612e667db65529e11a
zh <zh@qq.com>
2019-08-01 21:46:52 +0800

    add:创建新项目

---
+++ src/main/java/com/cqlyy/life/MyLifeCommunityApplication.java
@@ -0,0 +1,14 @@
+package com.cqlyy.life;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class MyLifeCommunityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyLifeCommunityApplication.class,args);
+    }
+}
